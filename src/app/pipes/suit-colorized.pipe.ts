1b002f9561dea838fdbf9a23c64b2f634d904542
Darley Krefta <darleykrefta@hotmail.com>
2022-05-15 08:48:26 -0300

    feat: changed suit name

--- src/app/pipes/suit-colorized.pipe.ts
+++ src/app/pipes/suit-colorized.pipe.ts
@@ -1,7 +1,7 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'SuitColorized',
+  name: 'suitColorized',
 })
 export class SuitColorizedPipe implements PipeTransform {
   transform(suit: string): unknown {
